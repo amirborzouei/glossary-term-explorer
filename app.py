d7ce3682db7d3ccc6232e458a4c6ffb2cf3ce72c
Amir R. Borzoueinia <amirrezaborzouei@gmail.com>
2025-06-12 14:26:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,11 @@ language = st.radio("Choose input language / Sprache wählen:", ["English", "Deu
 # Filter terms by language
 if language == "English":
     terms = df['En_Term'].dropna().unique()
-    selected_term = st.selectbox("Select a term:", sorted(terms))
     en_terms = ["-- Type / Select the Term --"] + sorted(df['En_Term'].unique())
     selected_en_term = st.selectbox("English Term", en_terms, index=0)
     match = df[df['En_Term'] == selected_term]
 else:
     terms = df['DE_Term'].dropna().unique()
-    selected_term = st.selectbox("Begriff auswählen:", sorted(terms))
     de_terms = ["-- Tippen / Wählen Sie den Begriff --"] + sorted(df['DE_Term'].unique())
     ed_de_term = st.selectbox("Deutscher Begriff", de_terms, index=0)
     match = df[df['DE_Term'] == selected_term]
